2bfa0b48a147640a90be366e5c1e0918b9e70efb
Juno Jo <juno@juno.pro>
2021-11-11 14:09:35 -0500

    event model

---
+++ server/models/event.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const EventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true
+    },
+    location: [
+      {
+        city: { type: String, trim: true },
+        state: { type: String, trim: true },
+        country: { type: String, trim: true }
+      }
+    ],
+    date: {
+      type: Date,
+      default: Date.now
+    },
+    description: {
+      type: String
+    },
+    artist: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    }
+  },
+  { timestamps: true }
+);
+
+const Event = mongoose.model('Event', EventSchema);
+
+module.exports = Event;
