097a656d7903c8a770c75efe1798ff68e692a24b
itslhun <nahuelghastin@outlook.com>
2021-11-13 10:57:57 +0100

    add client app origin

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://hoppscotch.io'],
+    origin: [process.env.CLIENT_APP_ORIGIN, 'https://hoppscotch.io'],
     credentials: true
   })
 );
