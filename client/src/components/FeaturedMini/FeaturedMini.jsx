2154c4fe6d960b89de1e22195a6caa8a69d2f6dc
itslhun <nahuelghastin@outlook.com>
2021-11-20 09:45:02 +0100

    add featured mini component

--- client/src/components/FeaturedMini/FeaturedMini.jsx
+++ client/src/components/FeaturedMini/FeaturedMini.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import './FeaturedMini.scss';
 
 function FeaturedMini(props) {
   console.log(props);
   return (
     <li className="FeaturedMini">
-      <img
-        src={props.artist.backgroundImg || 'https://source.unsplash.com/random'}
-        alt={props.artist.firstName}
-      />
+      {/* Link to artist public profile */}
+      <Link to={`/artists/${props.artist._id}`}>
+        <img
+          src={
+            props.artist.backgroundImg || 'https://source.unsplash.com/random'
+          }
+          alt={props.artist.firstName}
+        />
+        <figcaption>
+          {props.artist.firstName} {props.artist.lastName}
+        </figcaption>
+      </Link>
     </li>
   );
 }
