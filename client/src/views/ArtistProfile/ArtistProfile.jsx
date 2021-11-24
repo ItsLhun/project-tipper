c08409eeb3a7eec526ea08099125d2ae0a5cca9e
itslhun <nahuelghastin@outlook.com>
2021-11-24 19:57:38 +0100

    fix positioning of buttons on artist profile view

--- client/src/views/ArtistProfile/ArtistProfile.jsx
+++ client/src/views/ArtistProfile/ArtistProfile.jsx
@@ -13,6 +13,7 @@ import './ArtistProfile.scss';
 import settingsIcon from './cog.svg';
 import editIcon from './pencil.svg';
 import cameraIcon from './camera.svg';
+import signOutIcon from './sign-out.svg';
 
 function ArtistProfileView(props) {
   const [artist, setArtist] = useState();
@@ -248,74 +249,43 @@ function ArtistProfileView(props) {
         </form>
       )}
       {isOwnProfile && (
-        <button className="settings-btn" onClick={changeSettings}>
+        <button className="settings-btn top-icon" onClick={changeSettings}>
           <img src={settingsIcon} alt="settings" />
         </button>
       )}
       {isOwnProfile && (
-        <button className={'sign-out-btn'} onClick={props.onSignOut}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="#f5da00"
-            viewBox="0 0 512 512"
-          >
-            <path d="M10 9.408l2.963 2.592-2.963 2.592v-1.592h-8v-2h8v-1.592zm-2-4.408v4h-8v6h8v4l8-7-8-7zm6-3c-1.787 0-3.46.474-4.911 1.295l.228.2 1.396 1.221c1.004-.456 2.114-.716 3.287-.716 4.411 0 8 3.589 8 8s-3.589 8-8 8c-1.173 0-2.283-.26-3.288-.715l-1.396 1.221-.228.2c1.452.82 3.125 1.294 4.912 1.294 5.522 0 10-4.477 10-10s-4.478-10-10-10z" />
-          </svg>
+        <button className="sign-out-btn top-icon" onClick={props.onSignOut}>
+          <img src={signOutIcon} alt="sign out" />
         </button>
       )}
       <div className="UserProfileView_header">
-        <div>
-          <div className="BackgroundImgAndButton">
-            {artist?.backgroundImg ? (
-              <img
-                className={'Background'}
-                src={artist.backgroundImg}
-                alt="Artist pic"
-              />
-            ) : (
-              <img
-                className={'Background'}
-                src={'https://source.unsplash.com/400x500/?musician'}
-                alt="random musician"
-              />
-            )}
-
-            {isOwnProfile && (
-              <Link to="/artist/upload-background">
-                <button className={'background-btn'}>
-                  <svg
-                    version="1.1"
-                    id="Layer_1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 512 512"
-                  >
-                    <g>
-                      <g>
-                        <path
-                          d="M403.914,0L54.044,349.871L0,512l162.128-54.044L512,108.086L403.914,0z M295.829,151.319l21.617,21.617L110.638,379.745
-			l-21.617-21.617L295.829,151.319z M71.532,455.932l-15.463-15.463l18.015-54.043l51.491,51.491L71.532,455.932z M153.871,422.979
-			l-21.617-21.617l206.809-206.809l21.617,21.617L153.871,422.979z M382.297,194.555l-64.852-64.852l21.617-21.617l64.852,64.852
-			L382.297,194.555z M360.679,86.468l43.234-43.235l64.853,64.853l-43.235,43.234L360.679,86.468z"
-                        />
-                      </g>
-                    </g>
-                  </svg>
-                </button>
-              </Link>
-            )}
-          </div>
+        <div className="BackgroundImgAndButton">
+          {artist?.backgroundImg ? (
+            <img
+              className="Background"
+              src={artist.backgroundImg}
+              alt="Artist pic"
+            />
+          ) : (
+            <img
+              className="Background"
+              src={'https://source.unsplash.com/random/?musician'}
+              alt="random musician"
+            />
+          )}
+          {isOwnProfile && (
+            <Link to="/artist/upload-background" className="background-btn">
+              <img src={cameraIcon} alt="upload" />
+            </Link>
+          )}
         </div>
+
         <div className="name-avatar-bio-wrapper">
           <div className="UserProfileView_avatar_wrap">
             {artist?.avatarUrl ? (
               <>
                 <img
                   className="avatar-circle UserProfileView_avatar"
-                  // src={'https://source.unsplash.com/random'}
                   src={artist?.avatarUrl}
                   alt={'avatar'}
                 />
@@ -347,7 +317,7 @@ function ArtistProfileView(props) {
               </>
             )}
           </div>
-          {(artist?.bio && <span>{artist.bio}</span>) || <span>No bio</span>}
+          {(artist?.bio && <em>{artist.bio}</em>) || <em></em>}
         </div>
 
         <StarRating {...props} user={props.user} isLoggedIn={isLoggedIn} />
@@ -355,7 +325,7 @@ function ArtistProfileView(props) {
         <div className={'ArtistProfileView_follow'}>
           <div>
             <span>{count}</span>
-            <span>followers</span>
+            <span>{count === 1 ? 'follower' : 'followers'}</span>
           </div>
           {!isOwnProfile && follow && (
             <button className="unfollow-btn" onClick={followNow}>
