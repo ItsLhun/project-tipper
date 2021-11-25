036cb38263f79851e615937626562f061cb8bc79
itslhun <nahuelghastin@outlook.com>
2021-11-25 20:25:04 +0100

    create tip view

--- client/src/views/TipArtist/TipArtist.jsx
+++ client/src/views/TipArtist/TipArtist.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { loadArtist } from '../../services/artist';
 
 import './TipArtist.scss';
 
 function TipArtistView(props) {
+  const [artist, setArtist] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   props.onUserRefresh();
+  // }, [props.user]);
+
+  useEffect(() => {
+    loadArtist(props.match.params.id).then((artist) => {
+      setArtist(artist);
+      setLoading(false);
+    });
+  }, [props.match.params.id]);
+
+  useEffect(() => {
+    if (!props.user) {
+      props.onUserRefresh();
+    }
+    if (props.match.params.id === props.user?._id) {
+      props.history.push('/profile');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.match.params.id, props.user?._id, props.history]);
+
   return (
     <div className="TipArtistView">
-      <h1>TipArtist</h1>
+      <h2>Select amount</h2>
+      {loading ? (
+        <div>Loading details...</div>
+      ) : (
+        <>
+          <div className="artist-info">
+            <img src={artist.avatarUrl} alt={artist.firstName} />
+            <h2>
+              {artist.firstName} {artist.lastName}
+            </h2>
+            <em>{artist.bio}</em>
+          </div>
+          <div className="tip-amount">
+            <button>$ 1</button>
+            <button>$ 2</button>
+            <button className="tip-active">$ 5</button>
+            <button>$ 8</button>
+            <button>$ 10</button>
+            <button>...</button>
+          </div>
+        </>
+      )}
+      <button className="tip-btn">Send a Tip</button>
+      <div className="tip-warning">
+        This action is not reversible, please check the amount before tipping.
+      </div>
     </div>
   );
 }
