e258749681be7e50e3eda94270b5ea6f60dd0b59
itslhun <nahuelghastin@outlook.com>
2021-11-27 12:27:09 +0100

    fix event creation view width

--- client/src/views/Event/Create.jsx
+++ client/src/views/Event/Create.jsx
@@ -87,107 +87,111 @@ class CreateEventView extends Component {
 
   render() {
     return (
-      <div>
-        {(!this.state.showMap && (
-          <div className="CreateEventView">
-            <h1 className="section-title">New Event</h1>
+      <div className="CreateEventOutterWrapper">
+        <div>
+          {(!this.state.showMap && (
+            <div className="CreateEventView">
+              <h1 className="section-title">New Event</h1>
 
-            <form
-              onSubmit={this.handleFormSubmission}
-              className="CreateEventView_content_form"
-            >
-              <h3>Details</h3>
-              <div className="CreateEventView_inputs">
-                <label htmlFor="event-date">When</label>
-                <input
-                  id="event-date"
-                  name="duration"
-                  type="date"
-                  value={this.state.date}
-                  onChange={this.handleDateChange}
-                  placeholder="Date of the event"
-                />
-                <input
-                  id="event-time"
-                  name="time"
-                  type="time"
-                  value={this.state.time}
-                  onChange={this.handleTimeChange}
-                  placeholder="Date of the event"
-                />
-                <span
-                  className="right-now-btn event-btn"
-                  onClick={this.handleRightNowClick}
-                >
-                  Right Now!
-                </span>
-              </div>
-              <div className="CreateEventView_inputs">
-                <label htmlFor="event-title">Title</label>
-                <input
-                  id="event-title"
-                  name="title"
-                  type="text"
-                  value={this.state.title}
-                  onChange={this.handleInputChange}
-                  placeholder="Title for event"
-                />
-              </div>
-              <div className="CreateEventView_inputs">
-                <label htmlFor="event-description"></label>
-                <textarea
-                  id="event-description"
-                  name="description"
-                  placeholder="Add more info about your event"
-                  rows="5"
-                  maxLength="140"
-                  value={this.state.description}
-                  onChange={this.handleInputChange}
-                />
-              </div>
-
-              <div className="CreateEventView_inputs">
-                <label htmlFor="event-duration">Duration (minutes)</label>
-                <input
-                  id="event-duration"
-                  name="duration"
-                  type="number"
-                  placeholder="Duration of the event"
-                  value={this.state.duration}
-                  onChange={this.handleInputChange}
-                  min="5"
-                />
-              </div>
-
-              <h3>Location</h3>
-              <p>{this.state.address || 'Address... '}</p>
-              <span
-                className="select-location-btn event-btn"
-                onClick={this.handleMapToggle}
+              <form
+                onSubmit={this.handleFormSubmission}
+                className="CreateEventView_content_form"
               >
-                Select Location
-              </span>
+                <h3>Details</h3>
+                <div className="CreateEventView_inputs">
+                  <label htmlFor="event-date">When</label>
+                  <input
+                    id="event-date"
+                    name="duration"
+                    type="date"
+                    value={this.state.date}
+                    onChange={this.handleDateChange}
+                    placeholder="Date of the event"
+                  />
+                  <input
+                    id="event-time"
+                    name="time"
+                    type="time"
+                    value={this.state.time}
+                    onChange={this.handleTimeChange}
+                    placeholder="Date of the event"
+                  />
+                  <span
+                    className="right-now-btn event-btn"
+                    onClick={this.handleRightNowClick}
+                  >
+                    Right Now!
+                  </span>
+                </div>
+                <div className="CreateEventView_inputs">
+                  <label htmlFor="event-title">Title</label>
+                  <input
+                    id="event-title"
+                    name="title"
+                    type="text"
+                    value={this.state.title}
+                    onChange={this.handleInputChange}
+                    placeholder="Title for event"
+                  />
+                </div>
+                <div className="CreateEventView_inputs">
+                  <label htmlFor="event-description"></label>
+                  <textarea
+                    id="event-description"
+                    name="description"
+                    placeholder="Add more info about your event"
+                    rows="5"
+                    maxLength="140"
+                    value={this.state.description}
+                    onChange={this.handleInputChange}
+                  />
+                </div>
 
-              <h3>Genres</h3>
+                <div className="CreateEventView_inputs">
+                  <label htmlFor="event-duration">Duration (minutes)</label>
+                  <input
+                    id="event-duration"
+                    name="duration"
+                    type="number"
+                    placeholder="Duration of the event"
+                    value={this.state.duration}
+                    onChange={this.handleInputChange}
+                    min="5"
+                  />
+                </div>
 
-              <GenreCheckbox
-                options={this.props.definedGenres}
-                selected={this.state.genre}
-                onSelectedChange={this.handleGenreSelectionChange}
-              />
+                <h3>Location</h3>
+                <p>{this.state.address || 'Address... '}</p>
+                <span
+                  className="select-location-btn event-btn"
+                  onClick={this.handleMapToggle}
+                >
+                  Select Location
+                </span>
 
-              <button className="create-event-button event-btn">Confirm</button>
-            </form>
-          </div>
-        )) || (
-          <MapOverlay
-            isLoaded={this.props.isLoaded}
-            onClose={this.handleMapToggle}
-            onConfirmation={this.handleLocationChange}
-            userLocation={this.props.userLocation}
-            geocoder={this.props.geocoder}
-          />
-        )}
+                <h3>Genres</h3>
+
+                <GenreCheckbox
+                  options={this.props.definedGenres}
+                  selected={this.state.genre}
+                  onSelectedChange={this.handleGenreSelectionChange}
+                />
+
+                <button className="create-event-button event-btn">
+                  Confirm
+                </button>
+              </form>
+            </div>
+          )) || (
+            <MapOverlay
+              isLoaded={this.props.isLoaded}
+              onClose={this.handleMapToggle}
+              onConfirmation={this.handleLocationChange}
+              userLocation={this.props.userLocation}
+              geocoder={this.props.geocoder}
+            />
+          )}
+        </div>
       </div>
     );
   }
